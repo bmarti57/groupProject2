664e64fcf8e8639b35f6f4f4f2574bece366ecba
Brian Martin <Brian Martin>
2017-09-12 11:55:22 -0400

    package.json created, npm packages installed

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ var db = require("./models");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync({  }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
