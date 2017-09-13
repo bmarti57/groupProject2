f1e1cc193b87d79b7025cbd585b222465445eb51
Brian Martin <Brian Martin>
2017-09-13 10:11:49 -0400

    html and css linked, throw err module.js:471

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/#"));
+        res.sendFile(path.join(__dirname, "../public/welcomePage.html"));
     });
 
-    app.get("/#game_page", function(req, res) {
-        res.sendFile(path.join(__dirname, "..public/#"));
+    app.get("/game", function(req, res) {
+        res.sendFile(path.join(__dirname, "..public/questions.html"));
     });
 
     app.get("/#finish_page", function(req, res) {
