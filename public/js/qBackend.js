808121796a273b4bf4c211a4a149543d35dc4411
Caden Mayo <cadenmayo@gmail.com>
2017-09-14 13:45:31 -0400

    Added the beginning of backend and generation the questions

---
+++ public/js/qBackend.js
@@ -0,0 +1,41 @@
+// JavaScript source code
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var genres = ['Electronic', 'Folk', 'Jazz', 'Rock/Pop', 'Rap', 'Reggae', 'Religious', 'Vocal'];
+var decade = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020];
+var length = ['2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00'];
+
+var keyword;
+
+function genQuestion(qNo) {
+    if (qNo === 1 || qNo === 2) {
+        keyword = Math.floor(Math.random() * 25);
+        if (qNo === 1) {
+            $('#qH' + qNo).write('The artist name starts with ' + letters[keyword]);
+        }
+        else {
+            $('#qH' + qNo).write('The song title begins with ' + letters[keyword]);
+        }
+    }
+    else if (qNo === 5) {
+        keyword = Math.floor(Math.random() * 6);
+        $('#qH' + qNo).write('The song is over  ' + length[keyword] + ' minutes long.');
+    }
+    else if (qNo === 6 || qNo === 7) {
+        keyword = Math.floor(Math.random() * 7);
+        if (qNo === 6) {
+            $('#qH' + qNo).write('The song came out before ' + decade[keyword]);
+        }
+        else {
+            $('#qH' + qNo).write("The song's genre is  " + genres[keyword]);
+        }
+    }
+    else {
+        throw Error;
+    }
+};
+
+function databaseSearch(qNum, keystring, yes) {
+    switch (qNum) {
+        case 1:
+    }
+}
